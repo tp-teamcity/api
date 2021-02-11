c9e3b042379f14d0c13bed045290bd4f517d2526
BastienLaffon <bastien.laffon.pro@gmail.com>
2021-02-11 15:21:05 +0100

    add of unitTest

---
+++ api.test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using api;
+
+namespace api.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
