70364abff59c7a0319aa9f80f9106242f859a8b4
AlexLecq <alexandreleclercq1@outlook.fr>
2021-02-27 11:18:58 +0100

    fix foo

--- api/Controllers/UpperController.cs
+++ api/Controllers/UpperController.cs
@@ -29,7 +29,7 @@ namespace api.Controllers
             {
                 stringToUpper = reader.ReadToEndAsync().Result;
             }
-            return lib.Uppercase.ToUpper(stringToUpper) ;
+            return lib.Uppercase.ToUpper(stringToUpper);
         }
     }
 }
